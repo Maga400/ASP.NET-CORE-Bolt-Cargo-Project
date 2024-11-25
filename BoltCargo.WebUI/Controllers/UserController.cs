f55b066d5916166b6c991907c4d313d4588cdb5f
Hemze_jx83@itstep.edu.az <Hemze_jx83@itstep.edu.az>
2024-11-25 13:43:12 +0400

    write user role method

--- BoltCargo.WebUI/Controllers/UserController.cs
+++ BoltCargo.WebUI/Controllers/UserController.cs
@@ -251,5 +251,19 @@ namespace BoltCargo.WebUI.Controllers
             return NotFound(new { message = "No user found with this id" });
         }
 
+        [HttpGet("userRole/{id}")]
+        public async Task<IActionResult> GetUserRole(string id)
+        {
+            var user = await _customIdentityUserService.GetByIdAsync(id);
+            var userRole = await _userManager.GetRolesAsync(user);
+
+            if (user != null)
+            {
+                return Ok(new { Role = userRole });
+            }
+
+            return NotFound(new { Message = "No user found with this id" });
+        }
+
     }
 }
